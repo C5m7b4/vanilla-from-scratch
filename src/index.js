c8f898169d76653aab3dcd30e554604995e93086
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-08 11:41:32 -0600

    add style and basic state

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 console.log('coding is ready');
+import { data } from './data';
+import { isValid } from './utils';
 import './styles.css';
+
+let filteredData = data;
+
+const state = {
+  items: data,
+  currentItem: {
+    name: '',
+    size: '',
+    price: 0,
+    category: '',
+  },
+};
